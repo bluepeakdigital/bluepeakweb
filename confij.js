9d3b1ce6289275ccf45d3a0b480db069ef9eacd7
pbluepeakdigital <bluepeakdigital.team@gmail.com>
2025-12-22 14:23:48 +1200

    .

--- confij.js
+++ confij.js
@@ -3,4 +3,4 @@
 window.BP_API_BASE = "https://bluepeakweb.onrender.com";
 
 window.SUPABASE_URL = "https://wgpswmhjsgiiqrondoxv.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_mWqbuA7OWpuov2mlkB_FLQ_W1l1epKG";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndncHN3bWhqc2dpaXFyb25kb3h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzMjY0OTUsImV4cCI6MjA4MTkwMjQ5NX0.mG3Vknc6IkUUuzEWFrPSwYLeSCwBPxPCAijWzKAxRww";
