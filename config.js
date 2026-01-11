a82aac9d8a9d488091b28a3f79f8ef10b896d054
pbluepeakdigital <bluepeakdigital.team@gmail.com>
2026-01-11 20:22:19 +1200

    .

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-// config.js
-// Backend API base URL (Render)
+// config.js (frontend root)
 window.BP_API_BASE = "https://bluepeakweb.onrender.com";
 
-
-//backend keys
+// Supabase public keys (safe to expose)
 window.SUPABASE_URL = "https://wgpswmhjsgiiqrondoxv.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndncHN3bWhqc2dpaXFyb25kb3h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzMjY0OTUsImV4cCI6MjA4MTkwMjQ5NX0.mG3Vknc6IkUUuzEWFrPSwYLeSCwBPxPCAijWzKAxRww";
