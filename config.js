2686f4b9638eb0eee2ea11db7e092b528cd130c9
pbluepeakdigital <bluepeakdigital.team@gmail.com>
2026-01-11 20:15:31 +1200

    change the name

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// Backend API base URL (Render)
+window.BP_API_BASE = "https://bluepeakweb.onrender.com";
+
+
+//backend keys
+window.SUPABASE_URL = "https://wgpswmhjsgiiqrondoxv.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndncHN3bWhqc2dpaXFyb25kb3h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzMjY0OTUsImV4cCI6MjA4MTkwMjQ5NX0.mG3Vknc6IkUUuzEWFrPSwYLeSCwBPxPCAijWzKAxRww";
